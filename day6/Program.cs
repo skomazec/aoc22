d9cd93eb8c08066335e56f01acc4ccfba23d7c73
Srdjan Komazec <skomazec@gmail.com>
2022-12-06 11:54:52 +0100

    Solution to day 6

--- day6/Program.cs
+++ day6/Program.cs
@@ -1,3 +1,23 @@
 ﻿// https://adventofcode.com/2022/day/6
 
-Console.WriteLine("Where is the problem?");
+int DetectDistinct(int bufferLength)
+{
+    var buffer = new int[bufferLength];
+    int i = 0;
+
+    using var reader = new StreamReader(args[0]);
+    while(!reader.EndOfStream)
+    {
+        int current = reader.Read();
+        buffer[i % bufferLength] = current;
+        if (i > 3 && buffer.Distinct().Count() == bufferLength)
+        {
+            break;
+        }
+        i++;
+    }
+    return i + 1;
+}
+
+Console.WriteLine($"Puzzle 1 solution is : {DetectDistinct(4)}");
+Console.WriteLine($"Puzzle 2 solution is : {DetectDistinct(14)}");
